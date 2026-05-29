4ed9a95d7abaae85ba3ae6d621f5dc789a38ed40
Felipe de Souza <felipe.souza@sportbay.com.br>
2026-05-29 13:54:51 -0300

    Tela de lancamento codificada

--- app/src/main/java/com/example/trocatela/LancamentoActivity.kt
+++ app/src/main/java/com/example/trocatela/LancamentoActivity.kt
@@ -1,27 +1,34 @@
 package com.example.trocatela
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LancamentoActivity : AppCompatActivity() {
+
+    private lateinit var etCod: EditText
+    private lateinit var etQtd: EditText
+    private lateinit var etValor: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lancamento)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        etCod = findViewById(R.id.etCod)
+        etQtd = findViewById(R.id.etQtd)
+        etValor = findViewById(R.id.etValor)
     }
 
-    fun btLancamentoOnClick(view: View) {
-        val intent = Intent( this, LancamentoActivity::class.java)
-        startActivity(intent)
-    }
+    fun btConfirmarOnClick(view: View) {}
+    fun btListarOnClick(view: View) {}
 }
